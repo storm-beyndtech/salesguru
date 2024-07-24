23afc749fedb0ccba7cd296dcf5ee2ea0c945575
khameleonstorm <khameleonstorm@gmail.com>
2024-07-24 07:12:10 +0100

    bal

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -28,14 +28,19 @@ export default function Balance({ type, user }: BalanceProps) {
   };
 
   return (
-    <div className="p-2 bg-gray-100 dark:bg-gray-800 rounded-3xl">
-      <div className='bg-white rounded-3xl'>
-        <div className="min-w-80 bg-cover bg-center p-5 flex justify-between" style={{ backgroundImage: `url(${balBg})` }}>
-          <div>
+    <div className="p-2 bg-gray-100 dark:bg-gray-800 rounded-3xl w-[360px] max-sm:w-full">
+      <div className='bg-white rounded-3xl w-full'>
+        <div className="w-full bg-contain p-5 flex flex-col" style={{ backgroundImage: `url(${balBg})` }}>
+          <div className='flex justify-between items-center'>
             <p className="text-gray-400 font-semibold font-krub text-sm">
               Balance
             </p>
-            <h1 className="text-5xl font-krub font-medium text-black my-4">
+
+            <img className="w-11" src={miniLogo} alt="logo" />
+          </div>
+          
+
+            <h1 className="text-4xl font-krub font-[600] text-black my-4">
               <span className="text-xl text-gray-400 font-[Courier]">$</span>
               {type === 'balance'
                 ? formatBalance(user.balance)
@@ -44,16 +49,16 @@ export default function Balance({ type, user }: BalanceProps) {
                   : 0}
             </h1>
 
+
+
+          <div className="flex justify-between">
+            
             <Link
               to="/dashboard/withdrawal"
               className="bg-red-500/10 py-1.5 px-3 text-red-600 font-krub font-semibold rounded-full text-xs"
             >
               Withdraw
             </Link>
-          </div>
-
-          <div className="flex flex-col justify-between items-end">
-            <img className="w-11" src={miniLogo} alt="logo" />
 
             {type === 'bonus' ? (
               <div className="bg-blue-500/10 py-1.5 px-3 backdrop-blur-lg text-blue-600 font-krub font-semibold rounded-full text-xs" onClick={() => handleCopy(user.username)}>
